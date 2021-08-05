9bd24d7d2ccae4ad379de288dd33231f8b9d3869
Fabricio Bizotto <fabricio.bizotto@ifc.edu.br>
2021-08-04 22:45:24 -0300

    commit

--- src/main.js
+++ src/main.js
@@ -1,8 +1,5 @@
 import Vue from 'vue'
 import App from './App.vue'
-<<<<<<< HEAD
-import router from './router'
-=======
 import {router} from "./routes"
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 
@@ -14,7 +11,6 @@ import 'bootstrap-vue/dist/bootstrap-vue.css'
 Vue.use(BootstrapVue)
 // Optionally install the BootstrapVue icon components plugin
 Vue.use(IconsPlugin)
->>>>>>> main
 
 Vue.config.productionTip = false
 
@@ -22,9 +18,5 @@ Vue.config.productionTip = false
 
 new Vue({
   router,
-<<<<<<< HEAD
-  render: h => h(App)
-=======
   render: h => h(App),
->>>>>>> main
 }).$mount('#app')

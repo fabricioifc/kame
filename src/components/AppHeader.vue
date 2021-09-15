095c5e07278ccc31a73a43f10e64004fae16091e
Fabricio Bizotto <fabricio.bizotto@ifc.edu.br>
2021-09-15 10:39:54 -0300

    jogo bandeira

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,61 +1,69 @@
 <template>
   <header>
-    <img src="../assets/logo.png" alt="logo" height="40">
+    <img
+      src="../assets/logo.png"
+      alt="logo"
+      height="40"
+    >
     <p>Kamẽ</p>
     <nav id="nav">
       <ul>
-        <li><router-link to="/">Home</router-link></li>
-        <li><router-link to="/about">About</router-link></li>
-        <li><router-link to="/game">Game</router-link></li>
+        <li>
+          <router-link to="/">Home</router-link>
+        </li>
+        <li>
+          <router-link to="/about">About</router-link>
+        </li>
+        <li>
+          <router-link to="/game">Game</router-link>
+        </li>
       </ul>
     </nav>
   </header>
 </template>
 
 <script>
-
 export default {
-  name: 'app-header'
-}
+  name: "app-header",
+};
 </script>
 
 <style lang="scss" scoped>
+p {
+  color: #444;
+  font-family: "Trebuchet MS", "Lucida Sans Unicode", "Lucida Grande",
+    "Lucida Sans", Arial, sans-serif;
+}
+
+header {
+  display: flex;
+  border-bottom: 1px solid #ccc;
+  padding: 0.1rem 1rem;
 
   p {
-    color: #444;
-    font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
+    margin-left: 1rem;
   }
+  nav {
+    margin-left: auto;
 
-  header {
-    display: flex;
-    border-bottom: 1px solid #ccc;
-    padding: .1rem 1rem;
-
-    p {
-      margin-left: 1rem;
+    ul {
+      list-style: none;
     }
-    nav {
-      margin-left: auto;
 
-      ul {
-        list-style: none;
-      }
+    ul li {
+      display: inline-flex;
+      margin-left: 0.5rem;
+    }
 
-      ul li {
-        display: inline-flex;
-        margin-left: 0.5rem;
-      }
+    ul li a {
+      text-decoration: none;
+      color: #2c3e50;
+    }
 
-      ul li a {
-        text-decoration: none;
-        color: #2c3e50;
-      }
-
-      a.router-link-exact-active {
-        font-weight: bold;
-        color: #2c3e40
-      }
+    a.router-link-exact-active {
+      font-weight: bold;
+      color: #2c3e40;
     }
   }
-
+}
 </style>
--- src/views/Game.vue
+++ src/views/Game.vue
@@ -1,40 +1,68 @@
 <template>
-  <section>
-    
-    <div v-if="!status.pronto" class="carregando">
+  <section class="pt-5">
+
+    <div
+      v-if="!status.pronto"
+      class="carregando"
+    >
       <code>Carregando Jogo...</code>
     </div>
-    <div v-if="status.pronto && !status.iniciar" class="carregando">
+    <div
+      v-if="status.pronto && !status.iniciar"
+      class="carregando"
+    >
       <code><button @click="status.iniciar = true">Jogar</button></code>
     </div>
 
-    <div class="pergunta" v-show="status.pronto && status.iniciar">
+    <div
+      class="pergunta"
+      v-show="status.pronto && status.iniciar"
+    >
       <h1>Qual é a bandeira do contestado?</h1>
       <p><code>{{status.tentativas}} tentativas</code></p>
 
-      <span class="bandeiras"
+      <span
+        class="bandeiras"
         v-bind:key="item.code"
-        v-for="item in bandeiras">
-          <Bandeira :bandeira=item 
-            :class="!selecionada ? '' : item == selecionada ? 'selecionada' : 'outras'" 
-            @click="enviar"
-            />
+        v-for="item in bandeiras"
+      >
+        <Bandeira
+          :bandeira=item
+          :class="!selecionada ? '' : item == selecionada ? 'selecionada' : 'outras'"
+          @click="enviar"
+        />
       </span>
 
-      
-      <div class="verificar" v-show="selecionada != null && (!status.errou && !status.terminou)">
+      <div
+        class="verificar"
+        v-show="selecionada != null && (!status.errou && !status.terminou)"
+      >
         <hr>
-        <button @click="enviar" class="btn" type="button">Tem Certeza?</button>
+        <button
+          @click="enviar"
+          class="btn"
+          type="button"
+        >Tem Certeza?</button>
       </div>
     </div>
 
     <p>{{resultado}}</p>
 
-    <button v-show="status.errou" @click="play" class="btn btn-primary" type="button">
+    <button
+      v-show="status.errou"
+      @click="play"
+      class="btn btn-primary"
+      type="button"
+    >
       {{status.acertou ? 'Reiniciar o Jogo' : 'Tentar Novamente'}}
     </button>
 
-    <button v-show="status.terminou" @click="play" class="btn btn-primary" type="button">
+    <button
+      v-show="status.terminou"
+      @click="play"
+      class="btn btn-primary"
+      type="button"
+    >
       Reiniciar o Jogo
     </button>
 
@@ -42,12 +70,12 @@
 </template>
 
 <script>
-import Bandeira from '../components/Bandeira.vue'
-import getBandeirasRemote from '../services/bandeiras'
+import Bandeira from "../components/Bandeira.vue";
+import getBandeirasRemote from "../services/bandeiras";
 export default {
   props: [],
   components: { Bandeira },
-  name: 'Game',
+  name: "Game",
   data() {
     return {
       bandeiras: [],
@@ -60,94 +88,88 @@ export default {
         pronto: false,
         errou: false,
         tentativas: null,
-        terminou: false
-      }
-      
-    }
+        terminou: false,
+      },
+    };
   },
   created() {
-    this.status.pronto = false
-    this.play()
+    this.status.pronto = false;
+    this.play();
   },
   methods: {
-    
     async play() {
-      this.status.errou = false
+      this.status.errou = false;
       this.status.acertou = false;
       this.selecionada = null;
-      this.resultado = null
-      
+      this.resultado = null;
+
       if (!this.status.pronto || this.status.acertou || this.status.terminou) {
-        this.bandeiras = await getBandeirasRemote(10)
-        this.status.pronto = true
-        this.status.tentativas = 3
-        this.status.terminou = false
+        this.bandeiras = await getBandeirasRemote(10);
+        this.status.pronto = true;
+        this.status.tentativas = 3;
+        this.status.terminou = false;
       }
     },
-    escolher: function(bandeira) {
+    escolher: function (bandeira) {
       if (this.status.errou) {
-        return false
+        return false;
       }
 
-      const mesmaEscolha = this.selecionada && this.selecionada == bandeira
+      const mesmaEscolha = this.selecionada && this.selecionada == bandeira;
       if (mesmaEscolha) {
-        this.selecionada = null
-        return false
+        this.selecionada = null;
+        return false;
       }
-      this.selecionada = bandeira
-      console.log(this.selecionada)
+      this.selecionada = bandeira;
+      console.log(this.selecionada);
     },
 
-    enviar: function() {
+    enviar: function () {
       if (this.selecionada) {
-        this.status.acertou = this.selecionada.contestado
+        this.status.acertou = this.selecionada.contestado;
 
         if (!this.status.acertou) {
-          this.status.tentativas -=1
+          this.status.tentativas -= 1;
         }
 
         if (this.status.acertou) {
-          this.resultado = 'Parabéns! Essa é a bandeira do contestado!'
-          this.status.terminou = true
+          this.resultado = "Parabéns! Essa é a bandeira do contestado!";
+          this.status.terminou = true;
         } else if (this.status.tentativas == 0) {
-          this.resultado = 'Game Over!'
-          this.status.terminou = true
+          this.resultado = "Game Over!";
+          this.status.terminou = true;
         } else {
-          this.resultado = 'Que pena! Tente novamente!'
-          this.status.errou = true
+          this.resultado = "Que pena! Tente novamente!";
+          this.status.errou = true;
         }
       }
-      
-    }
-  }
-  
-}
+    },
+  },
+};
 </script>
 
 <style lang="scss" scoped>
+section {
+  margin: 0 auto;
+  text-align: center;
+}
 
-  section {
-    margin: 0 auto;
-    text-align: center;
-  }
+div.carregando {
+  margin: 2rem;
+  font-size: 2rem;
+}
 
-  div.carregando {
-    margin: 2rem;
-    font-size: 2rem;
-  }
+.bandeiras {
+  margin-right: 10px;
+}
 
-  .bandeiras {
-    margin-right: 10px;
-  }
+.bandeiras.active {
+  background-color: red;
+}
 
-  .bandeiras.active {
-    background-color: red;
-  }
-
-  button.btn {
-    margin: 0 auto;
-    display: block;
-    margin-top: 10px;
-  }
-
+button.btn {
+  margin: 0 auto;
+  display: block;
+  margin-top: 10px;
+}
 </style>

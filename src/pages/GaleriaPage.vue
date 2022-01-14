38c678d71169e771e316d9197686d4be1d4c131d
João Felix <jfeduardo14@gmail.com>
2022-01-14 13:50:23 -0300

    galeria

--- src/pages/GaleriaPage.vue
+++ src/pages/GaleriaPage.vue
@@ -21,23 +21,19 @@
           @sliding-end="onSlideEnd"
         >
           <!-- Text slides with image -->
-          <b-carousel-slide
-            caption="João Gualberto Gomes de Sá Filho"
-            text="Militar brasileiro bem sucessedido, graduado como Bacharel em Ciências Físicas e Matemáticas, foi prefeito de Curitiba, mas morreu na batalha do Irani."
-            :img-src="require('@/assets/images/gualberto.png')"
-          ></b-carousel-slide>
+          <b-carousel-slide :img-src="require('@/assets/images/gualberto.png')">
+            <h1>João Gualberto Gomes de Sá Filho</h1>
+          </b-carousel-slide>
 
           <b-carousel-slide
-            caption="Carlos Frederico de Mesquita Tertuliano Potiguara"
-            text="Militar que havia lutado na guerra dos canudos pelos brasileiros, que tentou um acordo sem êxito, depois atacou Santo Antonio que forçou os rebeldes fugirem."
             :img-src="require('@/assets/images/tertuliano.jpg')"
-          ></b-carousel-slide>
+          >
+            <h1>Carlos Frederico de Mesquita Tertuliano Potiguara</h1>
+          </b-carousel-slide>
 
-          <b-carousel-slide
-            caption="Hermes da fonseca contestado"
-            text="Hermes Rodrigues da Fonseca foi um militar e político brasileiro, presidente do Brasil entre 1910 e 1914. Foi o primeiro gaúcho a ser eleito presidente da República"
-            :img-src="require('@/assets/images/fonseca.jpg')"
-          ></b-carousel-slide>
+          <b-carousel-slide :img-src="require('@/assets/images/fonseca.jpg')">
+            <h1>Hermes Rodrigues da Fonseca</h1>
+          </b-carousel-slide>
         </b-carousel>
 
         <p class="mt-4">{{ sliding }}</p>
@@ -58,27 +54,56 @@
           @sliding-end="onSlideEnd"
         >
           <!-- Text slides with image -->
-          <b-carousel-slide
-            caption="José Maria de Santo Agostinho"
-            text="José Maria de Santo Agostinho, era um místico brasileiro do Estado de Santa Catarina."
-            :img-src="require('@/assets/images/imagem1.jpg')"
-          ></b-carousel-slide>
+          <b-carousel-slide :img-src="require('@/assets/images/imagem1.jpg')">
+            <h1>José Maria de Santo Agostinho</h1>
+          </b-carousel-slide>
 
           <!-- Slides with custom text -->
-          <b-carousel-slide
-            caption="Maria Rosa"
-            text="Maria Rosa, conhecida como “menina santa” foi uma das seguidoras mais fiéis e fervorosas do monge José Maria"
-            :img-src="require('@/assets/images/imagem2.jpeg')"
-          ></b-carousel-slide>
+          <b-carousel-slide :img-src="require('@/assets/images/imagem2.jpeg')">
+            <h1>Maria Rosa</h1>
+          </b-carousel-slide>
 
-          <b-carousel-slide
-            caption="Deodato Manoel Ramos"
-            text="Deodato Manuel Ramos, considerado pelos historiadores como o último líder dos contestadores. Adeodato transferiu o núcleo dos revoltosos para o vale de Santa Maria, que contava ainda com cerca de 50 mil homens."
-            :img-src="require('@/assets/images/adeodato.jpg')"
-          ></b-carousel-slide>
+          <b-carousel-slide :img-src="require('@/assets/images/adeodato.jpg')">
+            <h1>Deodato Manoel Ramos</h1>
+          </b-carousel-slide>
         </b-carousel>
 
         <p class="mt-4">{{ sliding }}</p>
+      </b-col>
+    </b-row>
+    <b-row>
+      <b-col cols="6" class="text-center mt-3">
+        <p class="museu-text font-weight-bold">
+          João Gualberto Gomes de Sá Filho Militar brasileiro bem sucessedido,
+          graduado como Bacharel em Ciências Físicas e Matemáticas, foi prefeito
+          de Curitiba, mas morreu na batalha do Irani.
+        </p>
+        <p class="museu-text font-weight-bold">
+          Frederico de Mesquita Tertuliano Potiguara Militar que havia lutado na
+          guerra dos canudos pelos brasileiros, que tentou um acordo sem êxito,
+          depois atacou Santo Antonio que forçou os rebeldes fugirem.
+        </p>
+        <p class="museu-text font-weight-bold">
+          Hermes Rodrigues da Fonseca foi um militar e político brasileiro,
+          presidente do Brasil entre 1910 e 1914. Foi o primeiro gaúcho a ser
+          eleito presidente da República.
+        </p>
+      </b-col>
+      <b-col cols="6" class="text-center mt-3">
+        <p class="museu-text font-weight-bold">
+          José Maria de Santo Agostinho, era um místico brasileiro do Estado de
+          Santa Catarina.
+        </p>
+        <p class="museu-text font-weight-bold">
+          Maria Rosa, conhecida como “menina santa” foi uma das seguidoras mais
+          fiéis e fervorosas do monge José Maria
+        </p>
+        <p class="museu-text font-weight-bold">
+          Deodato Manuel Ramos, considerado pelos historiadores como o último
+          líder dos contestadores. Adeodato transferiu o núcleo dos revoltosos
+          para o vale de Santa Maria, que contava ainda com cerca de 50 mil
+          homens
+        </p>
       </b-col>
     </b-row>
   </div>
